20cfd7085da666aaf4d9aa833306c565812fb57a
MightyDebbie <MightyDebbie>
2022-10-13 14:37:08 -0700

    getting to know betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - functions puts
+ * main - hello puts
  *
  * Return: 0
- */
+*/
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
-		return (0) ;
+	puts("\"Programming is like building is like multilingual puzzle");
+		return (0);
 }
