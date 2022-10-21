783af536ced535f9f0e047768394d40af5eeb56b
MightyDebbie <MightyDebbie>
2022-10-21 10:38:27 -0700

    my head is full

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,14 @@
  * prints_numbers - print the numbers from 0 to 9
  * Return: void
 */
-void print_numbers(void);
+void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		_putchar(c);
+	
 	}
 	_putchar('\n');
 }
