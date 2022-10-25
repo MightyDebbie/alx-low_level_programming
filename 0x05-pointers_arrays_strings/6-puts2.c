bef87b4be5240734d0d763b4d4740e4c9dbed9a3
MightyDebbie <MightyDebbie>
2022-10-24 19:53:16 -0700

    task six almost

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,8 @@
 #includde "main.h"
 
 /**
- * puts2 - print one char out of two string.
- * @str: input string
+ * puts2 - prints one char out of two string.
+ * @str: input string to check.`
  * Return: 0
 */
 
@@ -11,7 +11,9 @@ void puts2(char *str)
 	int string;
 
 	for (string = 0; str[string] != '\0'; string++)
+
 	if (string % 2 == 0)
 		_putchar(str[string]);
+
 	_putchar('\n');
 }
