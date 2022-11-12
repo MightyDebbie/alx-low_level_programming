ee35088b62efe43088a0172fe0c92b47224938c4
MightyDebbie <MightyDebbie>
2022-11-11 18:21:53 -0800

    exhausted is an understartment

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+  * _calloc - allocates an array memory of @nmemb elements
+  * @nmemb: number of members
+  * @size: size
+  *
+  * Return: ...
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int b;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+	for (b = 0; b < (nmemb * size); b++)
+		a[b] = 0;
+	return (a);
+}
