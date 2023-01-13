e5d38b8215919646cece1467bcd65697111e2afb
MightyDebbie <MightyDebbie>
2023-01-12 16:27:36 -0800

    Doubly linked

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -16,8 +18,9 @@ typedef struct dlistint_s
 	int n;
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
-}	dlistint_t;
+} dlistint_t;
 
+/* Function Prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -28,4 +31,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif/*LIST_H*/
+#endif /* LISTS_H */
